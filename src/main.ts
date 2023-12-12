0a751e2d0eb5479bf530fe425dc9dfc793256f99
vovkapultik <alpha@alphaguilty.io>
2023-12-12 12:49:02 +0200

    f

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,128 @@
-import express, { Request, Response , Application } from 'express';
-import dotenv from 'dotenv';
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
 
-import { generateWallet } from "./modules/aztec";
+import {
+  deployTokens,
+  generateWallet,
+  getOwnerWallet,
+  getPublicBalances,
+  getPrivateBalances,
+  loadWallet,
+  shieldBalance,
+  mintPublic,
+  mintPrivate,
+} from "./modules/aztec";
+import { BarretenbergSync } from "@aztec/bb.js";
 
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.get('/', async (req: Request, res: Response) => {
-  const wallet = await generateWallet();
-  res.send(JSON.stringify(wallet));
+app.use(express.json())
+
+app.get('/ownerWallet', async (req: Request, res: Response) => {
+  const address = (await getOwnerWallet()).getAddress().toString();
+
+  res.send({
+    address
+  });
 });
 
-app.listen(port, () => {
+app.post('/deployTokens', async (req: Request, res: Response) => {
+  const ownerWallet = await getOwnerWallet();
+  await deployTokens(ownerWallet);
+
+  res.send({
+    status: true
+  });
+});
+
+app.post('/generateWallet', async (req: Request, res: Response) => {
+  const wallet = await generateWallet();
+  console.log(`New account deployed at ${wallet.completeAddress.address}`);
+
+  res.send(wallet);
+});
+
+app.post('/loadWallet', async (req: Request, res: Response) => {
+  const wallet = await loadWallet(
+    req.body.encryptionPrivateKey,
+    req.body.signingPrivateKey,
+    req.body.completeAddress
+  );
+  res.send({
+    status: true
+  });
+});
+
+app.post('/publicBalances', async (req: Request, res: Response) => {
+  const balances = await getPublicBalances(
+      req.body.encryptionPrivateKey,
+      req.body.signingPrivateKey,
+      req.body.completeAddress
+  );
+
+  res.send({
+    balances
+  });
+});
+
+app.post('/privateBalances', async (req: Request, res: Response) => {
+  const balances = await getPrivateBalances(
+    req.body.encryptionPrivateKey,
+    req.body.signingPrivateKey,
+    req.body.completeAddress
+  );
+
+  res.send({
+    balances
+  });
+});
+
+app.post('/mintPublic', async (req: Request, res: Response) => {
+  const status = await mintPublic(
+    req.body.token,
+    req.body.amount,
+    req.body.encryptionPrivateKey,
+    req.body.signingPrivateKey,
+    req.body.completeAddress
+  );
+
+  res.send({
+    status
+  });
+});
+
+app.post('/mintPrivate', async (req: Request, res: Response) => {
+  const status = await mintPrivate(
+    req.body.token,
+    req.body.amount,
+    req.body.encryptionPrivateKey,
+    req.body.signingPrivateKey,
+    req.body.completeAddress
+  );
+
+  res.send({
+    status
+  });
+});
+
+app.post('/shieldBalance', async (req: Request, res: Response) => {
+  const balances = await shieldBalance(
+    req.body.token,
+    req.body.amount,
+    req.body.encryptionPrivateKey,
+    req.body.signingPrivateKey,
+    req.body.completeAddress
+  );
+
+  res.send({
+    balances
+  });
+});
+
+app.listen(port, async () => {
+  await BarretenbergSync.initSingleton();
   console.log(`Server is Fire at http://localhost:${port}`);
 });
--- src/modules/aztec/index.ts
+++ src/modules/aztec/index.ts
@@ -1,22 +1,189 @@
-import { createPXEClient, getSandboxAccountsWallets } from "@aztec/aztec.js";
+import {
+  Fq,
+  getSchnorrAccount,
+  PXE,
+  AccountWalletWithPrivateKey,
+  createPXEClient,
+  getSandboxAccountsWallets,
+  AztecAddressLike, AztecAddress
+} from "@aztec/aztec.js";
+import { CompleteAddress } from "@aztec/circuits.js";
+// @ts-ignore
+import { TokenContract } from "@aztec/noir-contracts/types";
+import { type } from "os";
 
 const pxe = createPXEClient(process.env.PXE_URL || "http://localhost:8080");
 
-export async function generateWallet() {
-  return getSandboxAccountsWallets(pxe)
+
+export async function getOwnerWallet() {
+  let ownerWallet: AccountWalletWithPrivateKey;
+  [ownerWallet] = await getSandboxAccountsWallets(pxe);
+
+  return ownerWallet;
 }
 
-//
-//
-// let ownerWallet: AccountWalletWithPrivateKey;
-// let dex: Contract, zkb: Contract, usdt: Contract;
-//
-// it('set owner', async () => {
-//   [ownerWallet] = await getSandboxAccountsWallets(pxe);
-// });
-//
-// it('deploy contracts', async () => {
-//   dex = await dexContract.deploy(ownerWallet, ownerWallet.getAddress()).send().deployed();
-// zkb = TokenContract.deploy(ownerWallet, ownerWallet.getAddress()).send().deployed();
-//   usdt = await TokenContract.deploy(ownerWallet, ownerWallet.getAddress()).send().deployed();
-// });
+export async function deployTokens(ownerWallet: AccountWalletWithPrivateKey) {
+  const WMATIC = await TokenContract.deploy(ownerWallet, ownerWallet.getAddress()).send().deployed();
+  const USDT = await TokenContract.deploy(ownerWallet, ownerWallet.getAddress()).send().deployed();
+
+  process.env.WMATIC_ADDRESS = WMATIC.address.toString();
+  process.env.USDT_ADDRESS = USDT.address.toString();
+}
+
+export async function generateWallet() {
+  const encryptionPrivateKey = Fq.random();
+  const signingPrivateKey = Fq.random();
+  const wallet = await getSchnorrAccount(
+    pxe,
+    encryptionPrivateKey,
+    signingPrivateKey
+  ).waitDeploy();
+
+  return {
+    encryptionPrivateKey: encryptionPrivateKey.toString(),
+    signingPrivateKey: signingPrivateKey.toString(),
+    completeAddress: {
+      address: wallet.getCompleteAddress().address.toString(),
+      partialAddress: wallet.getCompleteAddress().partialAddress.toString(),
+      publicKey: wallet.getCompleteAddress().publicKey.toString()
+    }
+  }
+}
+
+export async function loadWallet(encryptionPrivateKey: Fq, signingPrivateKey: Fq, completeAddress: CompleteAddress) {
+  const wallet = await getSchnorrAccount(
+    pxe,
+    new Fq(Number(encryptionPrivateKey)),
+    new Fq(Number(signingPrivateKey)),
+    new CompleteAddress(completeAddress.address, completeAddress.publicKey, completeAddress.partialAddress)
+  ).register();
+
+  console.log(wallet);
+  console.log(`getAddress: ${wallet.getAddress()}`);
+  console.log(wallet.getCompleteAddress());
+  console.log(`-----------------------------------`);
+  const ownerWallet = await getOwnerWallet();
+  console.log(ownerWallet);
+  console.log(`getAddress: ${ownerWallet.getAddress()}`);
+  console.log(ownerWallet.getCompleteAddress());
+  console.log(`-----------------------------------`);
+
+  return wallet
+}
+
+export async function getPublicBalances (encryptionPrivateKey: Fq, signingPrivateKey: Fq, completeAddress: CompleteAddress) {
+  const wallet = await loadWallet(encryptionPrivateKey, signingPrivateKey, completeAddress);
+
+  const WMATIC = await TokenContract.at(AztecAddress.fromString(process.env.WMATIC_ADDRESS), wallet);
+  const USDT = await TokenContract.at(AztecAddress.fromString(process.env.USDT_ADDRESS), wallet);
+
+  const wmaticBalance = await WMATIC.methods.balance_of_public(wallet.getCompleteAddress().address).view();
+  const usdtBalance = await USDT.methods.balance_of_public(wallet.getCompleteAddress().address).view();
+
+  return {
+    WMATIC: wmaticBalance.toString(),
+    USDT: usdtBalance.toString()
+  }
+}
+
+export async function getPrivateBalances (encryptionPrivateKey: Fq, signingPrivateKey: Fq, completeAddress: CompleteAddress) {
+  const wallet = await loadWallet(encryptionPrivateKey, signingPrivateKey, completeAddress);
+
+  const WMATIC = await TokenContract.at(AztecAddress.fromString(process.env.WMATIC_ADDRESS), wallet);
+  const USDT = await TokenContract.at(AztecAddress.fromString(process.env.USDT_ADDRESS), wallet);
+
+  const wmaticBalance = await WMATIC.methods.balance_of_private(wallet.getCompleteAddress().address).view();
+  const usdtBalance = await USDT.methods.balance_of_private(wallet.getCompleteAddress().address).view();
+
+  return {
+    WMATIC: wmaticBalance.toString(),
+    USDT: usdtBalance.toString()
+  }
+}
+
+export async function mintPublic (token: string, amount: number, encryptionPrivateKey: Fq, signingPrivateKey: Fq, completeAddress: CompleteAddress) {
+  const wallet = await loadWallet(encryptionPrivateKey, signingPrivateKey, completeAddress);
+  const ownerWallet = await getOwnerWallet();
+
+  let tokenContract;
+  let tokenOwnerContract;
+  if (token.toLowerCase() === 'wmatic') {
+    tokenContract = await TokenContract.at(AztecAddress.fromString(process.env.WMATIC_ADDRESS), wallet);
+    tokenOwnerContract = await TokenContract.at(AztecAddress.fromString(process.env.WMATIC_ADDRESS), ownerWallet);
+  } else if (token.toLowerCase() === 'usdt') {
+    tokenContract = await TokenContract.at(AztecAddress.fromString(process.env.USDT_ADDRESS), wallet);
+    tokenOwnerContract = await TokenContract.at(AztecAddress.fromString(process.env.USDT_ADDRESS), ownerWallet);
+  } else {
+    return {
+      status: false,
+      reason: 'No such token'
+    }
+  }
+  try {
+    const tx = tokenContract.methods.mint_public(wallet.getAddress(), amount).send();
+    const receipt = await tx.wait();
+    console.log(receipt.status)
+
+    // await tokenOwnerContract.methods.set_minter(wallet.getAddress(), true).send().wait();
+    // await tokenContract.methods.mint_public(wallet.getAddress(), BigInt(amount)).send().wait();
+    // await tokenOwnerContract.methods.set_minter(wallet.getAddress(), false).send().wait();
+
+    return {
+      status: true
+    }
+  } catch (e) {
+    console.log(e);
+    return {
+      status: false,
+      reason: e
+    }
+  }
+}
+
+export async function mintPrivate (token: string, amount: number, encryptionPrivateKey: Fq, signingPrivateKey: Fq, completeAddress: CompleteAddress) {
+  const wallet = await loadWallet(encryptionPrivateKey, signingPrivateKey, completeAddress);
+
+  // let tokenContract;
+  // if (token.toLowerCase() === 'wmatic') {
+  //   tokenContract = await TokenContract.at(AztecAddress.fromString(process.env.WMATIC_ADDRESS), wallet);
+  // } else if (token.toLowerCase() === 'usdt') {
+  //   tokenContract = await TokenContract.at(AztecAddress.fromString(process.env.USDT_ADDRESS), wallet);
+  // } else {
+  //   return {
+  //       status: false,
+  //       reason: 'No such token'
+  //     }
+  // }
+  //
+  // tokenContract.methods.shield(wallet.getAddress(), amount, )
+  //
+  // return {
+  //   WMATIC: wmaticBalance.toString(),
+  //   USDT: usdtBalance.toString()
+  // }
+}
+
+export async function shieldBalance (token: string, amount: number, encryptionPrivateKey: Fq, signingPrivateKey: Fq, completeAddress: CompleteAddress) {
+  const wallet = await loadWallet(encryptionPrivateKey, signingPrivateKey, completeAddress);
+
+  let tokenContract;
+  if (token.toLowerCase() === 'wmatic') {
+    tokenContract = await TokenContract.at(AztecAddress.fromString(process.env.WMATIC_ADDRESS), wallet);
+  } else if (token.toLowerCase() === 'usdt') {
+    tokenContract = await TokenContract.at(AztecAddress.fromString(process.env.USDT_ADDRESS), wallet);
+  } else {
+    return {
+      status: false,
+      reason: 'No such token'
+    }
+  }
+
+  return { status: true }
+
+  // tokenContract.methods.shield(wallet.getAddress(), amount, )
+
+  // return {
+  //   WMATIC: wmaticBalance.toString(),
+  //   USDT: usdtBalance.toString()
+  // }
+}
